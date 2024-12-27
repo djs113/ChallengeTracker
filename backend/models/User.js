06127ead4396aad430040b3ec4a960a2df5eeac0
Daniel <daniel.julius.sam.113@gmail.com>
2024-12-27 11:59:33 +0530

    added pasword hashing for user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,4 @@
-const { Password } = require('@mui/icons-material');
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
@@ -9,4 +9,32 @@ const UserSchema = new mongoose.Schema({
     challenges: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Challenge'}],
 });
 
-module.exports = mongoose.model('User', UserSchema);
+UserSchema.pre('save', async function(next) {
+    const user = this;
+
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(user.password, salt);
+        user.password = hashedPassword;
+        next();
+    } catch(error) {
+        next(error);
+    }
+});
+
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+    try {
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    } catch (error) {
+        throw new Error('Error comparing password');
+    }
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
