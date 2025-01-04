7ebf7cc38fdf694a54bc0fa72040c1389a1e0dd9
Daniel <daniel.julius.sam.113@gmail.com>
2025-01-04 09:22:16 +0530

    fixed tailwind css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./public/index.html"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
 
 
